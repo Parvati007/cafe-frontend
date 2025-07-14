ba4ad741b4ea0616baaff118130f8816c8190837
Parvati007 <parvati000777@gmail.com>
2025-07-14 14:16:51 +0530

    updated

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -1,30 +1,29 @@
-import React from 'react'
-import { useEffect,useState } from 'react'
-import axios from 'axios'
+import React from "react";
+import { useEffect, useState, useRef } from "react";
+import { Link } from "react-router-dom";
+import axios from "axios";
 
 export default function Users() {
-    const [users,setUsers]=useState([])
-    const [error, setError]=useState(null)
-    const API_URL=import.meta.env.VITE_API_URL
+  const [users, setUsers] = useState([]);
+  const [error, setError] = useState();
 
-    const fetchUsers = async()=>{
-      try{
-        setError("Loading...")
-        const url=`${API_URL}/api/users/showusers`
-        const result = await axios.get(url)
-        setUsers(result.data);
-        setError()
-      }catch(err){
-        console.log(err)
-        setError("Failed to fetch users. Please try again later.")
-      }
-    };
+  const API_URL = import.meta.env.VITE_API_URL;
+  const fetchUsers = async () => {
+    try {
+      const url = `${API_URL}/api/users/showusers`;
+      const result = await axios.get(url);
+      setUsers(result.data);
+    } catch (err) {
+      console.log(err);
+      setError("Something went wrong");
+    }
+  };
 
-    useEffect(()=>{
-        fetchUsers();
-    },[])
+  useEffect(() => {
+    fetchUsers();
+  }, []);
 
-    const handleDelete = async (id) => {
+  const handleDelete = async (id) => {
     try {
       const url = `${API_URL}/api/users/${id}`;
       const result = await axios.delete(url);
@@ -36,16 +35,42 @@ export default function Users() {
     }
   };
 
-    return (
+  return (
     <div>
-          {error && <p>{error}</p>}
-     <ul>
-         {users.map(user => (
-         <li key={user._id}>{user.firstName} 
-         <button onClick={() => handleDelete(user._id)}>Delete</button>
-         </li>
-          ))}
-     </ul>
+      <div>
+        <h2>User Management</h2>
+        <div>
+          <table border="1">
+            <thead>
+              <tr>
+                <th>First Name</th>
+                <th>Last Name</th>
+                <th>Email address</th>
+                <th>Role</th>
+         
+                <th>Action</th>
+              </tr>
+            </thead>
+            {users &&
+              users.map((user) => (
+                <tbody key={user._id}>
+                  <tr>
+                    <td>{user.firstName}</td>
+                    <td>{user.lastName}</td>
+                    <td>{user.email}</td>
+                    <td>{user.role}</td>
+                    <td>
+                      <button onClick={() => handleDelete(user._id)}>
+                        Delete
+                      </button>
+                    </td>
+                  </tr>
+                </tbody>
+              ))}
+            <tfoot></tfoot>
+          </table>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
