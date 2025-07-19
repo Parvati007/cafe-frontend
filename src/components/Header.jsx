687b7fdef8701c54421d463c9188e95124c986b7
Parvati007 <parvati000777@gmail.com>
2025-07-19 16:40:45 +0530

    updated

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,11 +4,9 @@ import { useContext } from "react";
 import App, { AppContext } from "../App";
 export default function Header() {
   const { user } = useContext(AppContext);
-  
   return (
     <div>
       <h1>MERN Frontend</h1>
-      
       <Link to="/">Home</Link>-<Link to="/cart">MyCart</Link>-
       <Link to="/order">MyOrder</Link>
 
